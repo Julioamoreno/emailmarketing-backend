3cb08f0f9d35a783c6cb2c78f0d8e58e2cffa888
Júlio Armando <julioarmando321@gmail.com>
2020-04-02 12:50:55 -0300

    :sparkles: Adcionando controller da campanha

---
+++ src/controllers/CampaignController.js
@@ -0,0 +1,65 @@
+const campaign = require('../models/campaign');
+
+const ServiceList = require('../services/Crud');
+const service = new ServiceList(campaign);
+
+const ListsController = {
+    show (req, res) {
+        service.list()
+            .then(result => {
+                return res.json(result);
+
+            })
+            .catch(err =>{
+                return res.status(400).json({err});
+            }
+        );
+    },
+
+    store(req, res) {
+        service.add(req.body)
+            .then(result => {
+                return res.json(result);
+            })
+            .catch(err =>{
+                return res.status(422).json({err: err});
+                
+            }
+        )
+    },
+    showOne (req, res) {
+
+        service.listOne(req.params.id)
+            .then(result => {
+                return res.json(result);
+
+            })
+            .catch(err =>{
+                return res.status(400).json({err});
+            }
+        );
+    },
+    edit (req, res) {
+        service.edit(req.params.id)
+            .then(result => {
+                return res.json(result);
+
+            })
+            .catch(err =>{
+                return res.status(400).json({err});
+            }
+        );
+    },
+    delete (req, res) {
+        service.delete(req.params.id)
+            .then(result => {
+                return res.json(result);
+
+            })
+            .catch(err =>{
+                return res.status(400).json({err});
+            }
+        );
+    },
+}
+module.exports = ListsController;
