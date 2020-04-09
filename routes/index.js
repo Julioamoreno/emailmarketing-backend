b7fbbe7f063aedef9d4d598d1fc6263d0ee53c12
Júlio Armando <julioarmando321@gmail.com>
2020-04-08 21:48:28 -0300

    :lock: Adcionando middleware nas rotas /api

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,17 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const passport = require('../src/auth/auth');
 
-var campaign = require('./campaign');
-var lead = require('./lead');
-var list = require('./list');
+const campaign = require('./campaign');
+const lead = require('./lead');
+const list = require('./list');
  
 router.get('/', (req, res) => {
   res.render('index', {title: 'express'})
 });
+
+//criando middleware para as rotas /api
+router.use('/api', passport.authenticate('jwt', {session: false}));
 
 router.use('/api', campaign);
 router.use('/api', lead);
