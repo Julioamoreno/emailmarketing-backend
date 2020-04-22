fdc78988c58abd0ea28694e9bb7928c69eb1f0a8
Júlio Armando <julioarmando321@gmail.com>
2020-04-22 01:42:41 -0300

    :art: Add funçao subscribe na lead

--- src/controllers/LeadsController.js
+++ src/controllers/LeadsController.js
@@ -1,9 +1,74 @@
-const lead = require('../models/lead');
+const leadModel = require('../models/lead');
+const listModel = require('../models/list');
 
+const { validationResult } = require('express-validator')
 const ServiceList = require('../services/Crud');
-const service = new ServiceList(lead);
+const service = new ServiceList(leadModel);
 
-const ListsController = {
+const LeadsController = {
+    async subscribe(req, res){
+
+        const err = validationResult(req);
+        if(!err.isEmpty()){
+            return res.status(422).json(err);
+        }else{
+            let lista;
+            //busca uma lista
+            await listModel.findOne({ title: req.body.list }, async (err, result) => { 
+                if(!result) { //verifica se essa lista não existe
+                    //cria uma lista
+                    await listModel.create({ title: req.body.list, quantity: 0}, (err, result) =>{
+                        lista = result; //._id  
+                    })
+                }else{  //caso a lista exista
+                    lista = result;  
+                }
+            })
+            let register = function() {
+                const lead = {
+                    email: req.body.email,
+                    lists: [lista._id]
+                }
+                leadModel.create(lead);
+
+                lista.quantity ++; //incrementa a quantidade da lista
+                lista.save();
+            }
+
+            let update = function(lead) {
+                if (lead.lists.indexOf(lista._id) === -1 ) { //verifica se não existe o ID
+                    lead.lists.push(lista._id); //adciona o id
+                    lead.save();
+
+                    lista.quantity ++; //incrementa a quantidade da lista
+                    lista.save();
+                }
+            }
+
+            leadModel.findOne({email: req.body.email}, function(err, lead) {
+                if (err) return res.json(err);
+                if (!lead) {
+                    register();
+                } else {
+                    console.log(lead);
+                    update(lead);
+                }
+            
+            });
+            leadModel.findOne({ email: req.body.email }).populate('lists').exec(function (err, res) {
+                console.log(res);
+            });
+
+            return res.json({status:'Sucesso'})
+        }
+    },
+    
+    leadsByList (req, res) {
+        leadModel.find({lists: {$in: [req.params.id] }}).populate('lists').exec(function (err, leads) {
+            return res.json(leads);
+        });
+    },
+
     show (req, res) {
         service.list()
             .then(result => {
@@ -62,4 +127,4 @@ const ListsController = {
         );
     },
 }
-module.exports = ListsController;
+module.exports = LeadsController;
