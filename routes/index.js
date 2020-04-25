112783be5d7915fb531642f512e08153974a5537
Júlio Armando <julioarmando321@gmail.com>
2020-04-25 19:13:20 -0300

    :art: Add arquivo das rotas de tracking

--- routes/index.js
+++ routes/index.js
@@ -6,12 +6,14 @@ const users = require('./users');
 const campaign = require('./campaign');
 const lead = require('./lead');
 const list = require('./list');
+const tracking = require('./tracking');
  
 router.get('/', (req, res) => {
   res.render('index', {title: 'express'})
 });
 
 router.use('/users', users);
+router.use(tracking);
 
 //criando middleware para as rotas /api
 router.use('/api', passport.authenticate('jwt', {session: false}));
