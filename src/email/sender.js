9f3dc555d677f21c45895a6e440960e037638c48
Júlio Armando <julioarmando321@gmail.com>
2020-05-13 16:06:22 -0300

    :art: Add variavel de ambiente e refatorando

--- src/email/sender.js
+++ src/email/sender.js
@@ -1,11 +1,11 @@
 const mailgun = require('mailgun-js');
-const DOMAIN = 'sandboxfc526c1ad7a34048a7ae481b14cde9af.mailgun.org';
+const DOMAIN = process.env.DOMAINMAILGUN;
 const mg = mailgun({domain: DOMAIN, apiKey: process.env.KEYMAILGUN});
 
 module.exports = function (to, subject, html) {
     return mg.messages()
-        .create({
-            from: 'Empresa teste <julioarmando@julioamoreno.com>',
+        .send({
+            from: 'Empresa teste ',
             to: [to],
             subject: subject,
             html: html,
